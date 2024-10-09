2aa7c3ff917ebdcd2ea06842fbc0026431b05718
Matias Raffo <matiasraffo90@gmail.com>
2024-10-09 17:48:10 -0300

    ok

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,10 @@
+// querySelector
+
+const heading = document.querySelector('.header__texto h2') //retorna a 0 o a 1 elemnto
+// console.log(heading);
+heading.textContent = 'Nuevo Heading';
+
+// querySelectorAll
+
+
+//getElemnentById
