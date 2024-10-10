73e41418241aed473d8a35d6aca4ad6248d7595b
Matias Raffo <matiasraffo90@gmail.com>
2024-10-10 17:39:38 -0300

    ok

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -1,10 +1,113 @@
 // querySelector
 
 const heading = document.querySelector('.header__texto h2') //retorna a 0 o a 1 elemnto
-// console.log(heading);
 heading.textContent = 'Nuevo Heading';
+console.log(heading);
 
 // querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
 
+
+enlaces[0].textContent = 'Nuevo texto para Enlace';
+// enlaces[0].href = 'http://www.google.com';
+enlaces[0].classList.add('nueva-clase');
+// enlaces[0].classList.remove('navegacion_enlace');
 
 //getElemnentById
+
+// const heading2 = document.getElementById('heading');
+// console.log(heading2);
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A')
+
+// agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+// agregar el texto
+nuevoEnlace.textContent = 'Tienda Virtual';
+// agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+// agregar al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+console.log(nuevoEnlace);
+
+
+
+
+
+
+// EVENTOS
+// console.log(1);
+
+// window.addEventListener('load', function() { // load espera a que el JS y los arhivos que dependen del html esten listo
+//     console.log(2);
+// } );
+
+// window.onload = function() {    
+//     console.log(3);
+// }
+
+// document.addEventListener('DOMContentLoaded', function() { // solo espera a que se descargue el html, pero no espera css o imagen. por eso imprime mas rapido.
+//     console.log(4);
+// });
+
+
+// console.log(5);
+
+// window.onscroll = function(e) {  // e es por evento
+//     console.log(e);
+// }
+
+// seleccionar elementos y asociarles un elemento.
+
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('click', function(e) {
+    console.log(e);
+    e.preventDefault();
+    // console.log('Enviando formulario');
+
+    // Validar un formulario
+    console.log('enviando formulario');
+});
+
+// el evento de submit
+
+const formulario = document.querySelector('.formulario');
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault('submit')
+})
+
+
+// Eventos de los inputs y textArea
+
+const datos = {
+    nombre:'',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+// nombre.addEventListener('input', function(e) {
+//     console.log(e.target.value);
+// } );
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+    // console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value;
+
+    // console.log(e.target)
+
+    console.log(datos);
+
+}
